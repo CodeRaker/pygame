e75e3d338dd56c3aa78f2a19d24f52397fb3d1c0
Peter Hecht Glad <pglad@EMEA.globalcsc.net>
2018-10-09 15:29:32 +0200

    test

---
+++ snow-example.py
@@ -0,0 +1,39 @@
+import pygame
+from random import randint
+from colorpalette import RGBColors
+from snowcannon import SnowCannon
+
+##
+# PYGAME INIT
+##
+pygame.init()
+rgb = RGBColors()
+screen_x = 1920
+screen_y = 1080
+screen = pygame.display.set_mode((screen_x, screen_y))
+pygame.display.set_caption("SnowFall")
+
+##
+# SnowCannon
+##
+snowCannon = SnowCannon(screen_x, screen_y, screen, rgb.white)
+snowCannon.snowFlakes_small = 25
+snowCannon.snowFlakes_large = 10
+snowCannon.spawn()
+
+clock = pygame.time.Clock()
+done = False
+
+while not done:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True
+
+    screen.fill(rgb.christmasblue)
+    snowCannon.animate()
+    snowCannon.blit()
+
+    pygame.display.flip()
+    clock.tick(60)
+
+pygame.quit()
